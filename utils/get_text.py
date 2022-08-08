10e124ae166921d1ae94873cca8bddd610d4bec7
jay <jay@correctionvitale.com>
2022-08-08 16:00:05 +0900

    mod, 텍스트 추출시 오류 개선

--- utils/get_text.py
+++ utils/get_text.py
@@ -1,12 +1,18 @@
+import logging
+
 import cv2
 import pytesseract
 
 
 def get_text(image):
-    options = "--psm 4"
-    # options = "--oem 3 --psm 6"
-    text = pytesseract.image_to_string(
-        cv2.cvtColor(image, cv2.COLOR_BGR2RGB), config=options, lang="kor"
-    )
-    text = text.replace(" ", "").replace("\n", "").replace("|", "").strip()
-    return text
+    try:
+        options = "--psm 4"
+        # options = "--oem 3 --psm 6"
+        text = pytesseract.image_to_string(
+            cv2.cvtColor(image, cv2.COLOR_BGR2RGB), config=options, lang="kor"
+        )
+        text = text.replace(" ", "").replace("\n", "").replace("|", "").strip()
+        return text
+    except Exception as ex:
+        logging.error(ex)
+        return ""
